789a189277c05fd431c059b7372531932e998946
ben <tuanminhdo1203@gmail.com>
2025-02-20 09:35:14 +0700

    Thêm chú thích

--- Features_Ex.py
+++ Features_Ex.py
@@ -3,8 +3,10 @@ import numpy as np
 
 class Feature_Extractor_Factory:
 
-    @staticmethod
-    def extract_color_histogram(image, bins=(8, 8, 8)):
+
+    @staticmethod #Static method giúp có thể gọi trực tiếp hàm này mà ko cần tạo object
+    # Hàm trích xuất histogram màu từ ảnh - Các tham số có thể điều chỉnh: bins
+    def extract_color_histogram(image, bins=(8, 8, 8)): 
         hist_lab = cv2.calcHist([cv2.cvtColor(image, cv2.COLOR_BGR2LAB)], [0, 1, 2], None, bins, [0, 256, 0, 256, 0, 256])
         hist_hsv = cv2.calcHist([cv2.cvtColor(image, cv2.COLOR_BGR2HSV)], [0, 1, 2], None, bins, [0, 256, 0, 256, 0, 256])
         hist_bgr = cv2.calcHist([image], [0, 1, 2], None, bins, [0, 256, 0, 256, 0, 256])
@@ -14,7 +16,10 @@ class Feature_Extractor_Factory:
         cv2.normalize(hist_bgr, hist_bgr)
         
         return np.hstack([hist_lab.flatten(), hist_hsv.flatten(), hist_bgr.flatten()])
+    
+
     @staticmethod
+    #Hàm trích xuất các đặc trưng về contour từ ảnh - Các tham số có thể điều chỉnh: None
     def extract_contour_features(image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         _, binary = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
@@ -32,12 +37,16 @@ class Feature_Extractor_Factory:
         perimeters = [cv2.arcLength(c, True) for c in contours]
         
         return np.array([sum(areas), sum(perimeters), len(contours)], dtype=np.float32), segmented_image
+    
+
     @staticmethod
+    #Hàm trích xuất các đặc trưng về hu_moments từ ảnh - Các tham số có thể điều chỉnh: None
     def extract_hu_moments(image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         moments = cv2.moments(gray)
         hu_moments = cv2.HuMoments(moments).flatten()
         return -np.sign(hu_moments) * np.log10(np.abs(hu_moments) + 1e-10)
+    
     @staticmethod
     def extract_feature(method,image, **kwargs):
         
